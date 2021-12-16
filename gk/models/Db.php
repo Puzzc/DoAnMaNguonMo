2934f4db537b51199f7240950e88b3c898439952
locnguyen2320 <93900868+locnguyen2320@users.noreply.github.com>
2021-12-16 10:06:29 +0700

    Định dạng câu truy vấn(CRUD)

--- gk/models/Db.php
+++ gk/models/Db.php
@@ -6,4 +6,20 @@ class Db{
         self::$pdo = new PDO('mysql:host='. HOST .';dbname='. DB, USER , PW);
         self::$pdo->query('set names utf8');
     }
-    ?>
+    protected function selectQuery($sql, $arr=[])
+    {
+        $stm = self::$pdo->prepare($sql);
+        $stm->execute($arr);
+        $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+
+    //insert, delete, update
+    protected function updateQuery($sql, $arr=[])
+    {
+        $stm = self::$pdo->prepare($sql);
+        $stm->execute($arr);
+        return $stm->rowCount();
+    }
+}
+?>
