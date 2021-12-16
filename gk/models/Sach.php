ba1cc9eb174472af2d77f843aee3f11551e53774
locnguyen2320 <93900868+locnguyen2320@users.noreply.github.com>
2021-12-16 10:11:15 +0700

    Viết truy vấn xuất ra tất cả sách và xuất ra sách ngẫu nhiên

--- gk/models/Sach.php
+++ gk/models/Sach.php
@@ -3,4 +3,14 @@ if (!defined('HOST')){
     exit;
 }
 class Sach extends Db 
-{}
+{
+    function all()
+    {
+        return $this->selectQuery('select * from sach');
+    }
+    
+    function random($n)
+    {
+        return $this->selectQuery("select * from sach order by rand() limit 0, $n");
+    }
+}
